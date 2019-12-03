34915cbc2dc02ef7a791fd3fd23c812b3cc4c7d4
mu-suke <chelsea_08ym@yahoo.co.jp>
2019-12-04 00:23:58 +0900

    contentful実装

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,3 +74,32 @@ export default {
     }
   }
 }
+
+const pkg = require('./package')
+const { getConfigForKeys } = require('./lib/config.js')
+const ctfConfig = getConfigForKeys([
+  'CTF_BLOG_POST_TYPE_ID',
+  'CTF_SPACE_ID',
+  'CTF_CDA_ACCESS_TOKEN'
+])
+
+const { createClient } = require('./plugins/contentful.js')
+const cdaClient = createClient(ctfConfig)
+
+module.exports = {
+  // 省略、、、
+  generate: {
+    routes() {
+      return cdaClient
+        .getEntries(ctfConfig.CTF_BLOG_POST_TYPE_ID)
+        .then(entries => {
+          return [...entries.items.map(entry => `/blog/${entry.fields.slug}`)]
+        })
+    }
+  },
+  env: {
+    CTF_SPACE_ID: ctfConfig.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: ctfConfig.CTF_CDA_ACCESS_TOKEN,
+    CTF_BLOG_POST_TYPE_ID: ctfConfig.CTF_BLOG_POST_TYPE_ID
+  }
+}
